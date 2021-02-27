1947933ca1e9e09d8cc118fa53a0ee8f7758605b
burkemm <burkemm84@gmail.com>
2021-02-26 20:43:59 -0600

    commit tree f2d9be0584cfe1abf95d59ba26243540aebf1356

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+def my_collect(collection) do |language|
+  return language.upcase
+end
+end
+
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+def my_collect(array)
+    i = 0
+    while i < array.length
+        yield array[i]
+        i = i + 1
+    end
+    array
+  
+end
+
 
 
